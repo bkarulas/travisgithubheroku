1ad1d8a4031d49091595b093e2c7333803174b32
Brad <bkarulas@outlook.com>
2020-01-09 21:29:35 -0500

    Fixed Issues

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /* server.js main file 1.4 */
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     // eslint-disable-next-line no-console
     console.log('req = \n', req);
     res.send('Test CI with Travis 1.0');
